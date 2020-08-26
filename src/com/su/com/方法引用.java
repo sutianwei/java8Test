78decdc5f326293582f3ee07ed74a229ddcd23a4
sutianwei <18700871054@163.com>
2020-08-26 22:22:48 +0800

    test

--- src/com/su/com/方法引用.java
+++ src/com/su/com/方法引用.java
@@ -13,7 +13,7 @@ public class 方法引用 {
             return file.isHidden();//筛选逻辑
         }
     });
-    //简易写法
+    //简易写法  重读了
     File[] hiddenFile = new File(".").listFiles(File::isHidden);
     //next
 }
