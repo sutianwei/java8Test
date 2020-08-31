610aa76f36730546f146130d756ddf3703c346c6
sutianwei <18700871054@163.com>
2020-08-31 23:07:24 +0800

    thread

---
+++ src/com/su/com/Apple.java
@@ -0,0 +1,22 @@
+package com.su.com;
+
+public class Apple {
+    private String Color;
+    private String Weight;
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public String getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(String weight) {
+        Weight = weight;
+    }
+}
---
+++ src/com/su/com/TestTimerOrder.java
@@ -0,0 +1,24 @@
+package com.su.com;
+
+public class TestTimerOrder {
+    public  static void main(String[] args) {
+        final long timeInteral =1000;
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println(Thread.currentThread().getName() + "tuime--- seconds");
+                    try {
+
+                        Thread.sleep(timeInteral);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+}
