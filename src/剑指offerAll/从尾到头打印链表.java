4996559c3b7a2046b5d507aec09d45d8fba34ff8
sutianwei <18700871054@163.com>
2020-10-21 22:46:36 +0800

    替换空格

---
+++ src/剑指offerAll/从尾到头打印链表.java
@@ -0,0 +1,36 @@
+package 剑指offerAll;
+
+import company.company.ListNode;
+
+import java.util.Stack;
+
+
+class ListNode1 {
+    int val;
+    ListNode1 next;
+
+    ListNode1(int x) {
+        val = x;
+    }
+}
+
+
+public class 从尾到头打印链表 {
+    public int[] reversePrint(ListNode1 head) {
+        /**
+         * 用栈存储数据，在导出到数组
+         */
+        Stack<ListNode1> stack = new Stack<>();
+        ListNode1 fast = head;
+        while (fast != null) {
+            stack.push(fast);
+            fast = fast.next;
+        }
+        int nums[] = new int[stack.size()];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = stack.pop().val;
+        }
+        return nums;
+
+    }
+}
