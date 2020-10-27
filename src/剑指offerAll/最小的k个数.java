5ff8a332a83bba76d153542ce3d28f590ac432e2
sutianwei <18700871054@163.com>
2020-10-27 23:35:47 +0800

    test

--- src/剑指offerAll/最小的k个数.java
+++ src/剑指offerAll/最小的k个数.java
@@ -11,7 +11,33 @@ public class 最小的k个数 {
             b[i] = arr[i];
         }
         return b;
-
-
     }
+
+
+
+    public int[] getLeastNumbers2(int[] arr, int k) {
+        int[] vec = new int[k];
+        if (k == 0) { // 排除 0 的情况
+            return vec;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            public int compare(Integer num1, Integer num2) {
+                return num2 - num1;
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            queue.offer(arr[i]);
+        }
+        for (int i = k; i < arr.length; ++i) {
+            if (queue.peek() > arr[i]) {
+                queue.poll();
+                queue.offer(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            vec[i] = queue.poll();
+        }
+        return vec;
+    }
+
 }
