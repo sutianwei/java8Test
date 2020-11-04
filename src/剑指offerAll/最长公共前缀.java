da3d515f66e24b11fe6f26613acd977f145c0d14
sutianwei <18700871054@163.com>
2020-11-05 00:12:39 +0800

    sdf

---
+++ src/剑指offerAll/最长公共前缀.java
@@ -0,0 +1,14 @@
+package 剑指offerAll;
+
+public class 最长公共前缀 {
+    /**
+     * 编写一个函数来查找字符串数组中的最长公共前缀。
+     *
+     * 如果不存在公共前缀，返回空字符串 ""。
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+
+    }
+}
