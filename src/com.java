b9f468f99884ffaa6829f61fc758f565eafdf3c9
sutianwei <18700871054@163.com>
2020-11-20 16:19:39 +0800

    test  true 1->2

--- src/com.java
+++ src/com.java
@@ -9,4 +9,10 @@ public class com {
     public void setName(String name) {
         this.name = name;
     }
+
+    public static void main(String[] args) {
+        Integer a=1;
+        Integer b =2;
+        System.out.println(a.equals(b));
+    }
 }
