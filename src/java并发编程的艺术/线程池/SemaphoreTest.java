e7822816369a0119c45cc99a39a3ea3895213c1a
sutianwei <18700871054@163.com>
2020-12-02 23:51:27 +0800

    创建对象的几种方式

---
+++ src/java并发编程的艺术/线程池/SemaphoreTest.java
@@ -0,0 +1,38 @@
+package java并发编程的艺术.线程池;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+    //停车场同时容纳的车辆10
+    private  static Semaphore semaphore=new Semaphore(10);
+
+    public static void main(String[] args) {
+
+        //模拟100辆车进入停车场
+        for(int i=0;i<40;i++){
+
+            Thread thread=new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        System.out.println("===="+Thread.currentThread().getName()+"来到停车场");
+                        if(semaphore.availablePermits()==0){
+                            System.out.println("车位不足，请耐心等待");
+                        }
+                        semaphore.acquire();//获取令牌尝试进入停车场
+                        System.out.println(Thread.currentThread().getName()+"成功进入停车场");
+                        Thread.sleep(new Random().nextInt(1000));//模拟车辆在停车场停留的时间
+                        System.out.println(Thread.currentThread().getName()+"驶出停车场");
+                        semaphore.release();//释放令牌，腾出停车场车位
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },i+"号车");
+
+            thread.start();
+
+        }
+
+    }
+}
