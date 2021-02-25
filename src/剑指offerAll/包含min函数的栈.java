d568da0ebb6c6424aba0a0d7a62eced222c8931f
sutianwei <18700871054@163.com>
2021-02-25 23:37:34 +0800

    包含min函数的栈

--- src/剑指offerAll/包含min函数的栈.java
+++ src/剑指offerAll/包含min函数的栈.java
@@ -1,22 +1,30 @@
 package 剑指offerAll;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.Stack;
 
 public class 包含min函数的栈 {
     /**
      * 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，
      * 调用 min、push 及 pop 的时间复杂度都是 O(1)。
      */
-    public static void main(String[] args) {
-        Lock lock = new ReentrantLock();
+    Stack<Integer> A = new Stack<>();
+    Stack<Integer> B = new Stack<>();
 
-        lock.lock();
-        try {
-
-        } finally {
-            lock.unlock();
-        }
+    public void push(int x) {
+        A.add(x);
+        if(B.empty() || B.peek() >= x)
+            B.add(x);
     }
+    public void pop() {
+        if(A.pop().equals(B.peek()))
+            B.pop();
+    }
+    public int top() {
+        return A.peek();
+    }
+    public int min() {
+        return B.peek();
+    }
+
 
 }
