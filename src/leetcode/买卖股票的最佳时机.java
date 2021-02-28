ef4184c4f6972ab282f13f7c1f4bf7a6ce9dec0f
sutianwei <18700871054@163.com>
2021-02-28 22:11:02 +0800

    可多次交易骨料

---
+++ src/leetcode/买卖股票的最佳时机.java
@@ -0,0 +1,82 @@
+package leetcode;
+
+public class 买卖股票的最佳时机 {
+    /**
+     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     * <p>
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * <p>
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     */
+
+    public static int maxProfit(int[] prices) {
+
+        if (prices.length == 0) return 0;
+        int res = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+
+            min = Math.min(min, prices[i - 1]);
+
+            res = Math.max(res, prices[i] - min);
+
+        }
+
+
+        return res;
+    }
+
+    public static int maxP(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        int max = 0;
+        int min = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else if (prices[i] - min > max) {
+                max = prices[i] - min;
+            }
+        }
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(a));
+
+    }
+
+    /**
+     * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+     * <p>
+     * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+     * <p>
+     * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public int test2(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        //0 CASH 1 STOCK
+        int[][] dp = new int[len][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+
+        return dp[len - 1][0];
+    }
+}
