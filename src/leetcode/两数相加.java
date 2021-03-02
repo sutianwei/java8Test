0d585f20c309bf49d1035d97c9745879cd07e97d
sutianwei <18700871054@163.com>
2021-03-02 23:04:15 +0800

    合格IP fix 合格IP

--- src/leetcode/两数相加.java
+++ src/leetcode/两数相加.java
@@ -6,17 +6,10 @@ public class 两数相加 {
 
     /**
      * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
-     *
+     * <p>
      * 请你将两个数相加，并以相同形式返回一个表示和的链表。
-     *
+     * <p>
      * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
-     *
-     * 来源：力扣（LeetCode）
-     * 链接：https://leetcode-cn.com/problems/add-two-numbers
-     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-     * @param l1
-     * @param l2
-     * @return
      */
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -48,12 +41,8 @@ public class 两数相加 {
     }
 
 
-
-
-
-
     public static void main(String[] args) {
-        System.out.println(4/ 10);
+        System.out.println(4 / 10);
         System.out.println(4 % 10);
     }
 }
