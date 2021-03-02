49f9ca073def7016b3b236a7bbc7c43c6faaec7f
sutianwei <18700871054@163.com>
2021-03-02 23:36:43 +0800

    完美数

---
+++ src/leetcode/完美数.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class 完美数 {
+
+    /**
+     * 完全数（Perfect number），又称完美数或完备数，是一些特殊的自然数。
+     * <p>
+     * 它所有的真因子（即除了自身以外的约数）的和（即因子函数），恰好等于它本身。
+     * <p>
+     * 例如：28，它有约数1、2、4、7、14、28，除去它本身28外，其余5个数相加，1+2+4+7+14=28。s
+     * <p>
+     * 输入n，请输出n以内(含n)完全数的个数。计算范围, 0 < n <= 500000
+     * <p>
+     * <p>
+     * 本题输入含有多组样例。
+     */
+
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+
+
+            int N = in.nextInt();
+            int count = 0;
+            for (int i = 1; i <= N; i++) {
+                int sum = 0;
+                for (int j = 1; j < i; j++) {
+                    if (i % j == 0) {
+                        sum += j;
+                    }
+                }
+                if (sum == i) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+
+
+    }
+}
