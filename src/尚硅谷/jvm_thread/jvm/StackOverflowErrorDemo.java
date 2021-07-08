417c76fe514fcd4f5bf677150a8cf1d04321e485
susu <18700871054@163.com>
2021-07-08 22:45:54 +0800

    尚硅谷jvm

---
+++ src/尚硅谷/jvm_thread/jvm/StackOverflowErrorDemo.java
@@ -0,0 +1,12 @@
+package 尚硅谷.jvm_thread.jvm;
+
+public class StackOverflowErrorDemo {
+    public static void main(String[] args) {
+        stackOverflowError();
+    }
+
+    private static void stackOverflowError() {
+        stackOverflowError();
+    }
+
+}
---
+++ src/尚硅谷/jvm_thread/jvm/StrongReferenceDemo.java
@@ -0,0 +1,11 @@
+package 尚硅谷.jvm_thread.jvm;
+
+public class StrongReferenceDemo {
+    public static void main(String[] args) {
+        Object o1=new Object();
+        Object o2=new Object();
+        o1=null;
+        System.gc();
+        System.out.println(o2);
+    }
+}
